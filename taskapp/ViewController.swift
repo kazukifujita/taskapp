a79a7b8e873814738cbaf85d1207866d84f23076
藤田 和樹 <fujitakazuki@imac.local>
2017-04-24 02:30:09 +0900

    メソッド組み込み

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return 0
 }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
+      return cell
+}
+   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+}
+ func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath)-> UITableViewCellEditingStyle {
+
+    return UITableViewCellEditingStyle.delete
+}
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+}
+}
